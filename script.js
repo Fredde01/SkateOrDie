250b194e935d0444157de6e8c1b5d19521de6eeb
olikal <oliverkalthoff@gmail.com>
2025-12-29 10:44:41 +0100

    mitars kod

---
+++ script.js
@@ -0,0 +1,33 @@
+function initContactForm() {
+  const contactForm = document.getElementById('contactForm');
+  const feedback = document.getElementById('formFeedback');
+
+  if(contactForm) {
+    contactForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      contactForm.style.display = 'none';
+      feedback.classList.remove('hidden');
+    })
+  }
+}
+
+
+
+
+function loadHeader() {
+    fetch('templates/fragments/header.html') 
+        .then(response => {
+            if (!response.ok) throw new Error("Kunde inte ladda headern");
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Fel:', error));
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadHeader();
+  initContactForm();
+});
